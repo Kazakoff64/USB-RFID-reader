8e0b2e195ddee0292df15a2babb6c0c6a74edc45
KazakOFF <kazakov.balakovo@gmail.com>
2020-03-18 09:45:17 +0400

    Добавьте файлы проекта.

---
+++ rfid/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace rfid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboComList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Read";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(24, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(406, 29);
+            this.textBox1.TabIndex = 3;
+            // 
+            // comboComList
+            // 
+            this.comboComList.FormattingEnabled = true;
+            this.comboComList.Location = new System.Drawing.Point(56, 24);
+            this.comboComList.Name = "comboComList";
+            this.comboComList.Size = new System.Drawing.Size(128, 21);
+            this.comboComList.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 160);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboComList);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "RFID reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboComList;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ rfid/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace rfid
+{
+    public partial class Form1 : Form
+    {
+
+        SerialPort sp = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                     
+            sp.PortName = comboComList.Text;
+            sp.BaudRate = 38400;
+            sp.Parity = Parity.None;
+            sp.DataBits = 8;
+            sp.StopBits = StopBits.One;
+            sp.Open();
+
+            //---------------------------------------------------------------------------------------
+
+            Thread.Sleep(40);
+
+            byte[] bytestosend = { 0xAA, 0xDD, 0x00, 0x03, 0x01, 0x02, 0x03 };
+            sp.Write(bytestosend, 0, bytestosend.Length);
+
+            Thread.Sleep(1);
+
+            byte[] bytestosend2 = { 0xAA, 0xDD, 0x00, 0x04, 0x01, 0x03, 0x0A, 0x08 };
+            sp.Write(bytestosend2, 0, bytestosend2.Length);
+
+
+        }
+
+
+        void RFRead() {
+        
+
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            while (true)
+            {
+
+                Thread.Sleep(100);
+
+                byte[] bytestosend4 = { 0xAA, 0xDD, 0x00, 0x03, 0x01, 0x0C, 0x0D };
+                sp.Write(bytestosend4, 0, bytestosend4.Length);
+
+                Thread.Sleep(60);
+
+                byte[] buffer = new byte[56];
+                int recv = sp.Read(buffer, 0, buffer.Length);
+
+                if (buffer[0].ToString() == "170" && buffer[1].ToString() == "221" && buffer[2].ToString() == "0" && buffer[3].ToString() == "9")
+                {
+                    if (buffer[7].ToString() != "0" && buffer[12].ToString() != "0" && buffer[11].ToString() != "0" && buffer[10].ToString() != "0")
+                    {
+
+                        string line = buffer[7].ToString() + " " + buffer[8].ToString() + " " + buffer[9].ToString() + " " + buffer[10].ToString() + " " + buffer[11].ToString() + " " + buffer[12].ToString();
+                        textBox1.Text = line;
+
+
+                        byte[] bytestosend2 = { 0xAA, 0xDD, 0x00, 0x04, 0x01, 0x03, 0x0A, 0x08 };
+                        sp.Write(bytestosend2, 0, bytestosend2.Length);
+
+
+                        break;
+
+                    }
+                }
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+     
+            //sp.Close();
+            //sp.Dispose();
+            //sp = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            foreach (String port in ports) {
+
+                comboComList.Items.Add(port);
+
+            }
+
+        }
+    }
+}
